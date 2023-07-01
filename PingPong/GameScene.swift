55ed8453429676ddc008422d4e78da4f8b943648
UserAvsr <arthurreis120@gmail.com>
2023-07-01 18:02:03 -0300

    PingPong SpriteKit

--- PingPong/GameScene.swift
+++ PingPong/GameScene.swift
@@ -10,79 +10,119 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var bola = SKSpriteNode()
+    var player = SKSpriteNode()
+    var oponente = SKSpriteNode()
     
+    var playerScore = SKLabelNode()
+    var oponenteScore = SKLabelNode()
+    
+    var score = (playerScore: 0, oponenteScore: 0)
+    
+    var forca: Int = 8
+
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        bola = self.childNode(withName: "bola") as! SKSpriteNode
+        
+        player = self.childNode(withName: "player") as! SKSpriteNode
+        player.position.x = (-self.frame.width/2) + 100
+
+        oponente = self.childNode(withName: "oponente") as! SKSpriteNode
+//        oponente.position.x = (self.frame.width / 2) + 170
+
+        playerScore = self.childNode(withName: "playerScore") as! SKLabelNode
+        oponenteScore = self.childNode(withName: "oponenteScore") as! SKLabelNode
+        
+        let borda = SKPhysicsBody(edgeLoopFrom: self.frame)
+        borda.friction = 0
+        borda.restitution = 1
+        self.physicsBody = borda
+        
+        startGame()
+        
+    }
+    
+    func startGame(){
+        
+        score.playerScore = 0
+        score.oponenteScore = 0
+        
+        updateScoreLabel()
+        bola.physicsBody?.applyImpulse(CGVector(dx: impulsoAleatorio() * forca, dy: impulsoAleatorio() * forca))
+    }
+    
+    func updateScoreLabel(){
+        
+        playerScore.text = String(score.playerScore)
+        oponenteScore.text = String(score.oponenteScore)
+        
+    }
+    
+    func addScore(playerQueMarcou: SKSpriteNode){
+        var impulso: CGVector!
+        
+        if (playerQueMarcou == player){
+            score.playerScore += 1
+            impulso = CGVector(dx: forca, dy: impulsoAleatorio() * forca)
+        }else {
+            score.oponenteScore += 1
+            impulso = CGVector(dx: -forca, dy: impulsoAleatorio() * forca)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        updateScoreLabel()
+        resetBall()
+        if(score.playerScore < 10 && score.oponenteScore < 10){
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(300)) { 
+                self.bola.physicsBody?.applyImpulse(impulso)
+            }
+
+        }
+    }
+    
+    func resetBall(){
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        bola.position = CGPoint(x: 0, y: 0)
+        bola.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    func impulsoAleatorio() -> Int{
+        
+        let numero = Int.random(in: 0...9)
+        return numero % 2 == 0 ? 1 : -1
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            if location.x < 0{
+                player.run(SKAction.moveTo(y: location.y, duration: 0.1))
+            }else if location.x > 0 {
+                oponente.run(SKAction.moveTo(y: location.y, duration: 0.1))
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches{
+            let location = touch.location(in: self)
+            
+            if location.x < 0{
+                player.run(SKAction.moveTo(y: location.y, duration: 0.1))
+            }else if location.x > 0 {
+                oponente.run(SKAction.moveTo(y: location.y, duration: 0.1))
+            }
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        if bola.position.x < player.position.x{
+            addScore(playerQueMarcou: oponente)
+        } else if bola.position.x > oponente.position.x{
+            addScore(playerQueMarcou: player)
+        }
     }
 }
